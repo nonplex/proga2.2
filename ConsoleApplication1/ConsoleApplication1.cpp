d2cd810d761efc45c14d0389b132c41123baa0f2
nonplex <fuksvladislav@gmail.com>
2023-09-25 01:36:20 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double x;
+	cout << "Кол-во студентов "; cin >> x;
+	int y;
+	cout << "Кол-во яблок "; cin >> y;
+	int delezhka;
+	if (x <= y)
+		//Проверка возможности выполнения условия
+		delezhka = y / x;//колво яблок на одного человека
+	else
+		cout << "Введенные данные не верны" << endl;
+	//В случае не подходящих условий
+	double otv = y - delezhka * x;
+	//Формула для расчета при верных условиях
+	cout << "Остаток яблок " << otv << endl;
+	//Вывод ответа
+}
